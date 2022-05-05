97aaf9176c2c3c6e3d9eb295ba6e79b6e8f25bde
forevermath@yandex.ru <forevermath@yandex.ru>
2022-05-05 23:28:21 +0300

    Добавьте файлы проекта.

---
+++ calc/main.cpp
@@ -0,0 +1,45 @@
+﻿#include "Complex.h"
+
+
+int main() {
+    Complex first(5, 10), second(10, 5);
+    Complex a = first + second;
+    a.getNum();
+    Complex b = first - second;
+    b.getNum();
+    Complex c = first / second;
+    c.getNum();
+    Complex d = first - second;
+    d.getNum();
+
+
+    ifstream file("complex.txt");
+    string temp;
+    getline(file, temp);
+    int n = stoi(temp);
+    cout << n << endl;
+    int x, y;
+    float max_module = -100000;
+    Complex maximum;
+    Complex* p = new Complex[n];
+    for (int i = 0; i < n; i++)
+    {
+        file >> x >> y;
+        Complex temp_second(x, y);
+        p[i] = temp_second;
+    }
+    file.close();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (p[i].module() > max_module)
+        {
+            max_module = p[i].module();
+            maximum = p[i];
+        }
+    }
+    cout << "Maximum module: " << max_module << endl;
+    cout << "Complex num: ";
+    maximum.getNum();
+    return 0;
+};
